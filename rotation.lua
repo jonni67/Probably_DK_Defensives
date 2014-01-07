1feb141f4c3348e97765fc6b07f3684af73278cd
jonni67 <j_swiecki@hotmail.com>
2014-01-06 21:11:16 -0500

    DK Defensives

---
+++ rotation.lua
@@ -0,0 +1,20 @@
+-- ProbablyEngine Rotation Packager
+-- Custom Blood Death Knight Rotation
+-- Created on Jan 7th 2014 1:58 am
+ProbablyEngine.rotation.register_custom(250, "DK Defensives", {
+
+  -- =======================================
+  --          Your Rotation Here!
+  -- =======================================
+  -- Everything between the first set of { }
+  -- in the core rotations can go here
+  -- =======================================
+  -- If you have any questions, please ask
+  -- on the ProbablyEngine Forums!
+  -- =======================================
+
+  -- Defensive Cooldowns
+  { "Vampiric Blood" , "modifier.cooldowns" , "player.health <= 20" },
+  { "Death Pact" , "modifier.cooldowns" , "player.health <= 20" },
+  
+})
