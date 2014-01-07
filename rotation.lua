fc7e85d70d2c447ca08b4d15dd71b72f9cd78b15
jonni67 <j_swiecki@hotmail.com>
2014-01-06 21:22:57 -0500

    added deathpact macro

--- rotation.lua
+++ rotation.lua
@@ -13,8 +13,16 @@ ProbablyEngine.rotation.register_custom(250, "DK Defensives", {
   -- on the ProbablyEngine Forums!
   -- =======================================
 
+  -- Death Pact Macro
+  { "!/cast Raise Dead\n/cast Death Pact", {
+    "player.health < 35",
+    "player.spell.cooldown(Death Pact)",
+    "player.spell.cooldown(Raise Dead)",
+    "player.spell.usable(Death Pact)"
+  }},
+  
   -- Defensive Cooldowns
-  { "Vampiric Blood" , "modifier.cooldowns" , "player.health <= 20" },
-  { "Death Pact" , "modifier.cooldowns" , "player.health <= 20" },
+  { "Vampiric Blood" , {"modifier.cooldowns" , "player.health <= 20"} },
+  { "Death Pact" , {"modifier.cooldowns" , "player.health <= 20"} },
   
 })
