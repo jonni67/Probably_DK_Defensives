492a1f483104c5f68dc07f6d26679f9200662231
jonni67 <j_swiecki@hotmail.com>
2014-01-06 21:29:58 -0500

    quick fix

--- rotation.lua
+++ rotation.lua
@@ -12,7 +12,11 @@ ProbablyEngine.rotation.register_custom(250, "DK Defensives", {
   -- If you have any questions, please ask
   -- on the ProbablyEngine Forums!
   -- =======================================
-
+  
+  -- Defensive Cooldowns
+  { "Vampiric Blood" , {"modifier.cooldowns" , "player.health <= 20"} },
+  { "Death Pact" , {"modifier.cooldowns" , "player.health <= 20"} },
+  
   -- Death Pact Macro
   { "!/cast Raise Dead\n/cast Death Pact", {
     "player.health < 20",
@@ -20,9 +24,5 @@ ProbablyEngine.rotation.register_custom(250, "DK Defensives", {
     "player.spell.cooldown(Raise Dead)",
     "player.spell.usable(Death Pact)"
   }},
-  
-  -- Defensive Cooldowns
-  { "Vampiric Blood" , {"modifier.cooldowns" , "player.health <= 20"} },
-  { "Death Pact" , {"modifier.cooldowns" , "player.health <= 20"} },
-  
+
 })
